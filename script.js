fd3e88bf11c8901e28724c80631018c7079522c0
Akash-kokate17 <Akashkokate1717@gmail.com>
2023-12-15 12:21:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// window.addEventListener('scroll', function() {
+//     var navContainer = document.querySelector('.nav-container');
+//     if (window.pageYOffset > 0) {
+//       navContainer.classList.add('fixed');
+//     } else {
+//       navContainer.classList.remove('fixed');
+//     }
+//   });
+  
